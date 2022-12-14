f31f8f4892208bf94165b5943f9c538e06e45db0
raylu15742cs <raylucs15742@gmail.com>
2022-12-14 14:36:32 -0800

    connection works

--- api/server.ts
+++ api/server.ts
@@ -13,7 +13,7 @@ require('dotenv').config();
 const dev_db_url =
   `mongodb+srv://admin:${process.env.DB_Password}@cluster0.titas9x.mongodb.net/?retryWrites=true&w=majority`;
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> console.log("we made contact"));
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
