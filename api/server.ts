a2fd584542a733c0e2849b2893a6b6137788618c
raylu15742cs <raylucs15742@gmail.com>
2022-12-14 14:36:06 -0800

    connect to mongo:

--- api/server.ts
+++ api/server.ts
@@ -6,3 +6,15 @@ const app = express()
 
 app.use(express.json())
 app.use(cors)
+
+require('dotenv').config();
+
+// Set up mongoose connection
+const dev_db_url =
+  `mongodb+srv://admin:${process.env.DB_Password}@cluster0.titas9x.mongodb.net/?retryWrites=true&w=majority`;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=> console.log("we made contact"));
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.listen(3000, () => console.log("were live"))
