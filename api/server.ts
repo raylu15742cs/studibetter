618cc2947a381dcd13ba14e08eb19078ba610f30
raylu15742cs <raylucs15742@gmail.com>
2022-12-15 12:34:42 -0800

    update typescript

--- api/server.ts
+++ api/server.ts
@@ -13,8 +13,11 @@ require('dotenv').config();
 const dev_db_url =
   `mongodb+srv://admin:${process.env.DB_Password}@cluster0.titas9x.mongodb.net/?retryWrites=true&w=majority`;
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).catch(console.error);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
+app.get('/' , async (req: Request,res:Response) => {
+  res.json()
+})
 app.listen(3000, () => console.log("were live"))
